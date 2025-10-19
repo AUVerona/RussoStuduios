1c6a6bed0cb0891b442157c30d3a723ca7eec8e8
Brubry2003 <info.auvr@gmail.com>
2025-10-19 16:37:14 +0200

    Reduce logo image size on Logo and LogoMaintenance
    Updated the logo image width in both Logo.tsx and LogoMaintenance.tsx components to use smaller dimensions for better layout and responsiveness.

--- workplace/src/app/components/LogoMaintenance.tsx
+++ workplace/src/app/components/LogoMaintenance.tsx
@@ -1,7 +1,7 @@
 export default function LogoMaintenance() {
   return (
     <div className="relative z-10 flex flex-col items-center mt-24 mb-8">
-      <img src="/logorussostudios.png" alt="Logo Russo Studios" className="w-[200px] sm:w-[700px] max-w-full h-auto drop-shadow-xl mb-0" />
+  <img src="/logorussostudios.png" alt="Logo Russo Studios" className="w-[120px] sm:w-[300px] max-w-full h-auto drop-shadow-xl mb-0" />
     </div>
   );
 }
