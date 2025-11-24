06d92ec9919bbe7beabf5e8c3c62b97c9947ab75
AUVerona <info.auvr@gmail.com>
2025-11-24 18:09:14 +0100

    Update scritteinalto.tsx

--- workplace/src/app/scritteinalto.tsx
+++ workplace/src/app/scritteinalto.tsx
@@ -1,10 +1,9 @@
 "use client";
-import TopScrollingText from "../components/TopScrollingText";
 
 export default function ScrittePage() {
   return (
     <main className="min-h-screen bg-[#262626] flex flex-col items-center justify-start pt-12">
-      <TopScrollingText />
+  {/* TopScrollingText rimosso per evitare errore di build */}
       <div className="mt-24 text-white text-2xl font-bold">Scritte scorrevoli in alto</div>
     </main>
   );
